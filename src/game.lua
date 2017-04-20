113497316fc996e0bd8cee1adf4a41af32023bd1
Rukinom <arichasting@gmail.com>
2017-04-20 17:26:05 -0500

    Added basic files

---
+++ src/game.lua
@@ -0,0 +1,13 @@
+game = {}
+
+function game:load(args)
+
+end
+
+function game:update(dt)
+
+end
+
+function game:draw()
+
+end]
---
+++ src/main.lua
@@ -0,0 +1,15 @@
+require "game"
+
+function love.load(args)
+	game.load(args)
+end
+
+
+function love.update(dt)
+	game.update(dt)
+end
+
+
+function love.draw()
+	game.draw()
+end
